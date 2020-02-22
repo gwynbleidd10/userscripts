94624b5e01d58508a0745fb13df32ac5d740cf02
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-02-23 03:47:48 +0900

    Add files via upload

--- ESEDtoTG.user.js
+++ ESEDtoTG.user.js
@@ -4,31 +4,23 @@
 // @version      0.8.1
 // @description  try to take over the world!
 // @author       Frey10
-// @match        *://esed.sakha.gov.ru/esed/WebRC*
-// @grant        none
+// @match        *://esed.sakha.gov.ru/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_listValues
 // @run-at       document-idle
 // ==/UserScript==
 
 //Настройки
-var username = ''; //username пользователя Telegram. Заполняется без @
-var time = 3; //Время в мс от начала загрузки страницы до появления панельки телеги
+var userID = '337277275'; //ID пользователя Telegram.
+var username = 'Вадим Рудых'; //Username пользователя Telegram. Заполняется без @
+var chatID = '337277275'; //ID группы пользователя Telegram.
 //
 
-(function() {
-    'use strict';
-    var i = setTimeout(function() {
-        addElem();
-    }, (time * 1000));
-})();
+//var $ = window.jQuery;
 
-
-function getRk(){
-    return document.getElementsByTagName('title')[0].innerText;
-}
-
-function pullTg(rk, chat, type){
-    var text = '', chatId = '337277275', url = 'рсмэв.рф';
-    if (chat == 'tak')
+function pullTg(rk, url, chat, type, text){
+    /*if (chat == 'tak')
     {
         text = '@MinaRe0 ' + type;
     }
@@ -39,9 +31,10 @@ function pullTg(rk, chat, type){
     else
     {
         text = type;
-    }
+    }*/
+    console.log(text);
     $.ajax({
-        url: 'http://esedtotg.herokuapp.com?chat=' + chatId + '&text=' + text + '&number=' + rk + '&url=' + url,
+        url: 'http://esedtotg.herokuapp.com?chat=' + chat + '&number=' + rk + '&userid=' + userID + '&username=' + username + '&type=' + type + '&text=' + text + '&url=' + url,
 //https://api.telegram.org/bot961112179:AAHjVaEbvUP7RHi_Pw4hIPtICfbaTzycT7c/sendMessage?chat_id=' + chatId + '&text=' + text + '&number=' + rk + '&url=' + url ,
         type: 'GET',
         dataType: 'html',
@@ -53,14 +46,161 @@ function pullTg(rk, chat, type){
 }
 
 $(document).ready(function() {
+    var doc_rc = new RegExp("^http:\/\/esed\.sakha\.gov\.ru\/esed\/WebRC\/DOC_RC\/DOC_RC\.aspx.+$");
+    var reply = new RegExp("^http:\/\/esed\.sakha\.gov\.ru\/esed\/WebRc\/RESOLUTION\/Reply\.aspx.+$");
+
+    var doc_rcpd = new RegExp("^http:\/\/esed\.sakha\.gov\.ru\/esed\/WebRc\/PRJ_RC\/PRJ_RC\.aspx.+$");
+    var visa = new RegExp("^http:\/\/esed\.sakha\.gov\.ru\/esed\/WebRc\/VISA_SIGN\/ADD_VS\.aspx.+$");
+    var visa_send = new RegExp("^http:\/\/esed\.sakha\.gov\.ru\/esed\/WebRc\/VISA_SIGN\/SEND\.aspx.+$");
 
 
-    $("div#Title-Place").on("click", "#tg-self-otchet", function () {
-        pullTg(getRk(), 'group', 'ввел(а) отчет');
+
+
+    var rk, url;
+
+    console.log('-------------------PAGE LOAD-------------------');
+    console.log(document.location.href);
+
+    /*
+    *   Проверка текущей страницы
+    */
+
+    //Проверка страницы РК
+    if (doc_rc.test(document.location.href)) {
+        console.log('-------------------DOC_RC START-------------------');
+        var i_doc_rc = setInterval(function() {
+            console.log('1-1');
+            if (document.querySelector('div.currentFile') != null) {
+                setValue();
+                clearInterval(i_doc_rc);
+                addElem();
+                console.log('-------------------DOC_RC LOAD-------------------');
+            }
+        }, (50));
+    }
+
+    //Проверка страницы РКПД
+    if (doc_rcpd.test(document.location.href)) {
+        console.log('-------------------DOC_RCPD START-------------------');
+        var i_doc_rcpd = setInterval(function() {
+            console.log('1-2');
+            if (document.querySelector('div.currentFile') != null) {
+                setValue();
+                clearInterval(i_doc_rcpd);
+                addElem();
+                console.log('-------------------DOC_RCPD LOAD-------------------');
+            }
+        }, (50));
+    }
+
+    /*
+    *   Проверка всплывающих окон
+    */
+
+    // + Проверка страницы отчёта
+    if (reply.test(document.location.href)) {
+        console.log('-------------------REPLY START-------------------');
+        var i_reply = setInterval(function() {
+            console.log('2-1');
+            if (document.querySelector('div[data-action=".Save"') != null) {
+                clearInterval(i_reply);
+                var btn = document.querySelector('div[data-action=".Save"');
+                btn.setAttribute('id', 'Save-btn-reply');
+                btn.removeAttribute('data-action');
+                console.log('-------------------REPLY LOAD-------------------');
+            }
+        }, (50));
+    }
+
+    //Проверка страницы визы
+    if (visa.test(document.location.href) || visa_send.test(document.location.href)) {
+        console.log('-------------------VISA START-------------------');
+        var i_visa = setInterval(function() {
+            console.log('2-2');
+            if (document.querySelector('div[data-action=".APPLY 1"') != null) {
+                clearInterval(i_visa);
+                var btn = document.querySelector('div[data-action=".APPLY 1"');
+                btn.setAttribute('id', 'Send-btn-visa');
+                btn.removeAttribute('data-action');
+                console.log('-------------------VISA LOAD-------------------');
+            }
+        }, (50));
+    }
+
+    /*
+    *   Обработчик отчёта
+    */
+
+    // + Обработчик отчёта, получение отчёта
+    $('body').on("click", '#Save-btn-reply', function () {
+        pullTg(window.opener.document.title, window.opener.location.href.replace(/#/gi, '%23'), chatID, 'answer', document.getElementById('16_REPLY_List_REPLY_TEXT').value);
+        var btn = document.getElementById('Save-btn-reply');
+        btn.setAttribute('data-action', '.Save');
+        btn.removeAttribute('id');
+        setTimeout(function() {
+            btn.click();
+        }, (500));
     });
-    $("div#Title-Place").on("click", "#tg-self-visa", function () {
-        pullTg(getRk(), 'group', 'завизировал(а)');
+
+    /*
+    *   Обработчик визы
+    */
+
+    //Обработчик визы, направление на визу
+    $('body').on("click", '#Send-btn-visa', function () {
+        var str = '', btn;
+        var slc = '', txt = '';
+        if (visa.test(document.location.href))
+        {
+            slc = document.querySelector('.simpleList.ui-sortable').textContent.trim().length > 0;
+            txt = document.querySelector('.simpleList.ui-sortable').innerText.split('\n').map(Function.prototype.call, String.prototype.trim).toString();
+        }
+        else
+        {
+            slc = document.querySelector('div.content .row .clipped').textContent.trim().length > 0;
+            txt = document.querySelector('div.content .row .clipped').textContent.trim().toString();
+        }
+        if (slc)
+        {
+            if (document.getElementById('3_undefined_PERIOD').value.length > 0)
+            {
+                pullTg(window.opener.document.title, window.opener.location.href.replace(/#/gi, '%23'), chatID, 'visa-send', txt);
+                btn = document.getElementById('Send-btn-visa');
+                btn.setAttribute('data-action', '.APPLY 1');
+                btn.removeAttribute('id');
+                setTimeout(function() {
+                    btn.click();
+                }, (500));
+            }
+            else
+            {
+                alert('Необходимо указать срок визирования.')
+            }
+        }
+        else
+        {
+            alert('Необходимо добавить визирующих.')
+        }
     });
+
+    /*var iter = document.querySelector('.simpleList.ui-sortable').querySelectorAll('.simpleItem');
+    var str = '';
+    for(var i = 0; i < iter.length; i++){
+    str += iter[i].querySelector('.singleline').querySelector('.singleline').textContent.trim() + '\n';
+    }
+    console.log(str);
+    */
+
+            //document.getElementById('3_undefined_PERIOD').value.length > 0
+
+            //Обработчики РК
+
+
+            /*$("div#Title-Place").on("click", "#tg-self-visa", function () {
+         var btn = document.getElementById('tg-self-visa');
+        btn.setAttribute('data-action', '.Ввести_отчёт');
+        btn.removeAttribute('id');
+    });*/
     $("div#Title-Place").on("click", "#tg-self-podpis", function () {
         pullTg(getRk(), 'group', 'подписал(а)');
     });
@@ -81,10 +221,10 @@ $(document).ready(function() {
 function addElem(){
     Element.prototype.appendBefore = function (element) {
         element.parentNode.insertBefore(this, element);
-    }, false;
+    };
     Element.prototype.appendAfter = function (element) {
         element.parentNode.insertBefore(this, element.nextSibling);
-    }, false;
+    };
 
     var tgBlock = document.getElementById('titleInfo');
     tgBlock.innerHTML = '';
@@ -96,24 +236,15 @@ function addElem(){
     tgRes.setAttribute("style", "display: inline; float: left;");
     tgBlock.appendChild(tgRes);
 
-    //3 вида
-    var so = document.createElement('input');
-    so.id = 'tg-self-otchet';
-    so.class = 'ribbonItem';
-    so.setAttribute("type", "button");
-    so.setAttribute("style", "margin: 0; border-right: 1px solid #d7d7d2; border-top: 1px solid #d7d7d2; border-bottom: 1px solid blue; height: 45px; box-sizing: border-box; color: #505050; cursor: pointer; line-height: 13px; vertical-align: middle; width: 120px; background-color: #aab0b1;");
-    so.setAttribute("value", "Ввел(а) отчет");
-    tgRes.appendChild(so);
-
-    var sv = document.createElement('input');
+    //2 вида
+    var sv = document.createElement('div');
     sv.id = 'tg-self-visa';
+    sv.innerHTML = 'asd';
     sv.class = 'ribbonItem';
-    sv.setAttribute("type", "button");
-    sv.setAttribute("style", "margin: 0; border-right: 1px solid #d7d7d2; border-top: 1px solid #d7d7d2; border-bottom: 1px solid blue; height: 45px; box-sizing: border-box; color: #505050; cursor: pointer; line-height: 13px; vertical-align: middle; width: 120px; background-color: #aab0b1;");
-    sv.setAttribute("value", 'Завизировал(а)');
-    sv.appendAfter(document.getElementById('tg-self-otchet'));
+    sv.setAttribute('data-action', '.Ввести_отчёт');
+    tgRes.appendChild(sv);
 
-    var sp = document.createElement('input');
+    /*var sp = document.createElement('input');
     sp.id = 'tg-self-podpis';
     sp.class = 'ribbonItem';
     sp.setAttribute("type", "button");
@@ -153,5 +284,14 @@ function addElem(){
     kma.setAttribute("type", "button");
     kma.setAttribute("style", "margin: 0; border-right: 1px solid #d7d7d2; border-top: 1px solid #d7d7d2; border-bottom: 1px solid blue; height: 45px; box-sizing: border-box; color: #ffffff; cursor: pointer; line-height: 13px; vertical-align: middle; width: 120px; background-color: #d95757;");
     kma.setAttribute("value", 'Карамзина М.А.');
-    kma.appendAfter(document.getElementById('tg-podpis-tak'));
+    kma.appendAfter(document.getElementById('tg-podpis-tak'));*/
+}
+
+function getRk(){
+    return document.getElementsByTagName('title')[0].innerText;
+}
+
+function setValue(){
+    GM_setValue('RK', getRk());
+    GM_setValue('URL', document.location.href);
 }
