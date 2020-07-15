fb14d262b10e31bb7d96434bec45b094f09d902a
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-07-15 16:38:46 +0900

    path to version

--- ESEDtoTG.user.js
+++ ESEDtoTG.user.js
@@ -29,34 +29,36 @@ const mode = 'prod' //prod, debug
 let checked = false;
 let outdated = false;
 let apiPath = '';
-if ((typeof userid != 'undefined') && (userid != ''))
-{
+if ((typeof userid != 'undefined') && (userid != '')) {
     checked = true;
 }
-else
-{
+else {
     addSettings();
 }
 
-if (mode == 'debug')
-{
+/*
+------------------------------------------------------------------------------------------------------
+-------------------------------------------Режим-скрипта----------------------------------------------
+------------------------------------------------------------------------------------------------------
+*/
+
+if (mode == 'debug') {
     apiPath = 'http://localhost:3000/api/esed';
+    versionPath = 'http://localhost:3000/api/esed/version';
 }
-else
-{
+else {
     apiPath = 'https://botsnode.herokuapp.com/api/esed';
+    versionPath = 'https://botsnode.herokuapp.com/api/esed/version';
 }
 
-
-
- /*
+/*
 ------------------------------------------------------------------------------------------------------
 ------------------------------------------Начало-скрипта----------------------------------------------
 ------------------------------------------------------------------------------------------------------
 */
 
-if (checked){
-    $(document).ready(function() {
+if (checked) {
+    $(document).ready(function () {
         let btn = '';
 
         let doc_rc = new RegExp(/^https?:\/\/esed\.sakha\.gov\.ru\/esed\/webrc\/doc_rc\/doc_rc\.aspx.+$/i);
@@ -83,12 +85,11 @@ if (checked){
         //Проверка страницы РК и РКПД
         if (doc_rc.test(document.location.href) || doc_rcpd.test(document.location.href)) {
             console.log('-------------------DOC START-------------------');
-            let i_doc_rc = setInterval(function() {
+            let i_doc_rc = setInterval(function () {
                 console.log('1-1');
                 if (document.querySelector('div.currentFile') != null) {
                     let author = [];
-                    for (let i = 0; i < document.querySelectorAll('div.lstItem a').length; i++)
-                    {
+                    for (let i = 0; i < document.querySelectorAll('div.lstItem a').length; i++) {
                         author.push(document.querySelectorAll('div.lstItem a')[i].text.trim());
                     }
                     addHiddenElem(author.toString(),);
@@ -105,20 +106,10 @@ if (checked){
         // Проверка страницы отчёта
         if (reply.test(document.location.href)) {
             console.log('-------------------REPLY START-------------------');
-            let i_reply = setInterval(function() {
+            let i_reply = setInterval(function () {
                 console.log('2-1');
                 if (document.querySelector('div[data-action=".Save"') != null) {
                     clearInterval(i_reply);
-
-                    /*
-                    let node = document.createElement('div');
-                    node.innerHTML = 'Не оповещать (Ознакомление)    <input type="checkbox" id="test" style="all:initial">';
-                    //node.innerHTML = 'Не оповещать (Ознакомление)    <select><option>Нет</option><option>Да</option></select><input type="radio"><style></style>';
-                    //node.innerHTML = '<input type="radio" id="contactChoice1" name="contact" value="email"><label for="contactChoice1">Email</label><input type="radio" id="contactChoice2" name="contact" value="phone"><label for="contactChoice2">Phone</label>';
-                    document.querySelector('.repStatus.ctrlHolder').appendChild(node);
-                    //document.querySelector('body').appendChild(node);
-                    */
-                    
                     let btn = document.querySelector('div[data-action=".Save"');
                     btn.setAttribute('id', 'Save-btn-reply');
                     btn.removeAttribute('data-action');
@@ -130,7 +121,7 @@ if (checked){
         //Проверка страницы отправки визы и подписи
         if (visa.test(document.location.href) || visa_send.test(document.location.href)) {
             console.log('-------------------VISA_PODPIS_SEND START-------------------');
-            let i_visa = setInterval(function() {
+            let i_visa = setInterval(function () {
                 console.log('2-2');
                 if (document.querySelector('div[data-action=".APPLY 1"') != null) {
                     clearInterval(i_visa);
@@ -145,7 +136,7 @@ if (checked){
         //Проверка страницы визы и подписи
         if (visa_podpis.test(document.location.href)) {
             console.log('-------------------VISA_PODPIS START-------------------');
-            let i_visa_podpis = setInterval(function() {
+            let i_visa_podpis = setInterval(function () {
                 console.log('2-3');
                 if (document.querySelector('div[data-action=".Save"') != null) {
                     clearInterval(i_visa_podpis);
@@ -165,33 +156,28 @@ if (checked){
         //  Обработчик отчёта, получение отчёта
         $('body').on("click", '#Save-btn-reply', async function () {
             //Из РК
-            if (doc_rc.test(window.opener.location.href) || doc_rcpd.test(window.opener.location.href))
-            {
+            if (doc_rc.test(window.opener.location.href) || doc_rcpd.test(window.opener.location.href)) {
                 let data = {};
                 data.title = window.opener.document.title;
                 data.url = window.opener.location.href;
                 data.type = "answer";
                 let a = document.querySelector('.repStatus.ctrlHolder').getElementsByTagName('select');
-                if (a[0].selectedIndex == '-1' || a[0].selectedIndex == '0' )
-                {
+                if (a[0].selectedIndex == '-1' || a[0].selectedIndex == '0') {
                     data.status = 'Не выбран';
                 }
-                else
-                {
+                else {
                     data.status = a[0].options[a[0].selectedIndex].text;
                 }
-                if (document.querySelector('[id$=_REPLY_List_REPLY_TEXT').value.length > 0)
-                {
+                if (document.querySelector('[id$=_REPLY_List_REPLY_TEXT').value.length > 0) {
                     data.text = document.querySelector('[id$=_REPLY_List_REPLY_TEXT').value.trim();
                 }
                 data.author = document.querySelector('.cl.DEPARTMENT.cab').innerText;
                 data.from = userid;
-                if (!outdated){
+                if (!outdated) {
                     await pullTg(data);
                 }
             }
-            if (mode == 'prod')
-            {
+            if (mode == 'prod') {
                 btn = document.getElementById('Save-btn-reply');
                 btn.setAttribute('data-action', '.Save');
                 btn.removeAttribute('id');
@@ -211,49 +197,40 @@ if (checked){
             data.title = window.opener.document.title;
             data.url = window.opener.location.href;
             //Определение типа
-            if (podpis.test(document.title))
-            {
+            if (podpis.test(document.title)) {
                 data.type = 'sign-send';
             }
-            else
-            {
+            else {
                 data.type = 'visa-send'
             }
             //Адресаты
-            if (visa.test(document.location.href))
-            {
+            if (visa.test(document.location.href)) {
                 list = document.querySelector('.simpleList.ui-sortable').textContent.trim().length > 0;
                 data.list = document.querySelector('.simpleList.ui-sortable').innerText.split('\n').map(Function.prototype.call, String.prototype.trim).toString();
             }
-            else
-            {
+            else {
                 list = document.querySelector('div.content div.row p.clipped').textContent.trim().length > 0;
                 data.list = document.querySelector('div.content div.row p.clipped').textContent.trim().toString();
             }
-            if (list)
-            {
+            if (list) {
                 //Срок
-                if (document.getElementById('3_undefined_PERIOD').value.length > 0)
-                {
+                if (document.getElementById('3_undefined_PERIOD').value.length > 0) {
                     data.from = userid;
-                    if (!outdated){
+                    if (!outdated) {
                         await pullTg(data);
                     }
-                    if (mode == 'prod')
-                    {
+                    if (mode == 'prod') {
                         btn = document.getElementById('Send-btn-visa');
                         btn.setAttribute('data-action', '.APPLY 1');
                         btn.removeAttribute('id');
                         btn.click();
                     }
                 }
-                else
-                {
+                else {
                     alert('Необходимо указать срок.')
                 }
             }
-            else
-            {
+            else {
                 alert('Необходимо добавить адресатов.')
             }
         });
@@ -270,27 +247,23 @@ if (checked){
             data.title = window.opener.document.title;
             data.url = window.opener.location.href;
             data.author = window.opener.document.getElementById('HiddenElement').attributes['author'].value;
-            if (podpis.test(document.title))
-            {
+            if (podpis.test(document.title)) {
                 data.type = 'sign';
                 data.status = $('input[name=signType]:checked').prop('labels')[1].innerText.trim();
             }
-            else
-            {
+            else {
                 data.type = 'visa';
                 let a = document.querySelector('.repStatus.ctrlHolder').getElementsByTagName('select');
                 data.status = a[0].options[a[0].selectedIndex].text;
             }
-            if (document.querySelector('[id$=_PRJ_VISA_SIGN_List_REP_TEXT').value.length > 0)
-            {
+            if (document.querySelector('[id$=_PRJ_VISA_SIGN_List_REP_TEXT').value.length > 0) {
                 data.comment = document.querySelector('[id$=_PRJ_VISA_SIGN_List_REP_TEXT').value.trim();
             }
             data.from = userid;
-            if (!outdated){
+            if (!outdated) {
                 await pullTg(data);
             }
-            if (mode == 'prod')
-            {
+            if (mode == 'prod') {
                 btn = document.getElementById('Send-btn-visa-sign');
                 btn.setAttribute('data-action', '.Save');
                 btn.removeAttribute('id');
@@ -300,19 +273,18 @@ if (checked){
     });
 }
 
-async function pullTg(data){
+async function pullTg(data) {
     await $.ajax({
         url: apiPath,
         type: 'POST',
         data: data,
-        success: function(res)
-        {
+        success: function (res) {
             console.log('Ответ: ' + res.status);
         }
     });
 }
 
-function addHiddenElem(author){
+function addHiddenElem(author) {
     var node = document.createElement('div');
     node.setAttribute('style', 'display:none;');
     node.setAttribute('id', 'HiddenElement');
@@ -320,7 +292,7 @@ function addHiddenElem(author){
     document.getElementById('aspnetForm').appendChild(node);
 }
 
-function addSettings(){
+function addSettings() {
     var html = '<form id="addForm">Введите UserID полученный от бота: <input type="text" id="addUserid" style="width: 100%;"/></form>';
     var node = document.createElement('div');
     node.setAttribute('style', 'display:none;');
@@ -328,22 +300,26 @@ function addSettings(){
     node.setAttribute('title', 'Настройка');
     node.innerHTML = html;
     document.getElementById('aspnetForm').appendChild(node);
-    $("#elementId").dialog({buttons:{"Сохранить":function(){
-        if (document.getElementById('addUserid').value != ''){
-            GM_setValue('tg_userID', document.getElementById('addUserid').value);
-            $(this).dialog("close");
+    $("#elementId").dialog({
+        buttons: {
+            "Сохранить": function () {
+                if (document.getElementById('addUserid').value != '') {
+                    GM_setValue('tg_userID', document.getElementById('addUserid').value);
+                    $(this).dialog("close");
+                }
+            }
         }
-    }}},{
-        modal:true,
-        width:400,
-        height:220
+    }, {
+        modal: true,
+        width: 400,
+        height: 220
     });
-    if (GM_getValue('tg_userID') != '' && typeof GM_getValue('tg_userID') != 'undefined'){
+    if (GM_getValue('tg_userID') != '' && typeof GM_getValue('tg_userID') != 'undefined') {
         document.getElementById('addUserid').value = GM_getValue('tg_userID');
     }
 }
 
-function addUpdate(current, actual){
+function addUpdate(current, actual) {
     let html = '<form id="addFormUpdate">Версия скрипта устарела. Текущая версия: <b>' + current + '</b>. Актуальная верся: <b>' + actual + '</b>. Уведомления не будут уходить пока не обновите скрипт!</form>';
     let node = document.createElement('div');
     node.setAttribute('style', 'display:none;');
@@ -352,34 +328,35 @@ function addUpdate(current, actual){
     node.innerHTML = html;
     document.getElementById('aspnetForm').appendChild(node);
     $("#updateBlock").dialog({
-        buttons:{
-            "Закрыть":function(){
+        buttons: {
+            "Закрыть": function () {
                 $(this).dialog("close");
             },
-            "Обновить":function(){
-                //$(this).dialog("close");
+            "Обновить": function () {
                 let win = window.open(scriptUrl);
                 win.focus();
             }
-        }},{
-        modal:true,
-        width:400,
-        height:220
+        }
+    }, {
+        modal: true,
+        width: 400,
+        height: 220
     });
 }
 
-function getVersion(){
+function getVersion() {
     $.ajax({
-        url: apiPath,
+        url: versionPath,
         type: 'get',
-        success: function(res)
-        {
+        success: function (res) {
             console.log('Версия: ' + res.version);
-            if ( GM_info.script.version != res.version){
+            if (GM_info.script.version != res.version) {
                 outdated = true;
                 addUpdate(GM_info.script.version, res.version)
             }
         },
-        error: function(err){alert(err)}
+        error: function (err) {
+            console.log(err)
+        }
     });
 }
