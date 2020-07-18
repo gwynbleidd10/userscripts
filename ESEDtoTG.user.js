dcf37cd0018f944939ce5f4abbfed956d1732d52
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-07-18 20:08:14 +0900

    Update ESEDtoTG.user.js

--- ESEDtoTG.user.js
+++ ESEDtoTG.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ESEDtoTG
 // @namespace    http://tampermonkey.net/
-// @version      2.6
+// @version      2.8
 // @description  try to take over the world!
 // @author       Frey10
 // @match        *://esed.sakha.gov.ru/*
